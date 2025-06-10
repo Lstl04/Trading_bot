2219738a10ad4280d30636f33d71883b5030fcbb
Louis Stalet <staletlouis@gmail.com>
2025-06-10 10:18:22 -0400

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -204,28 +204,3 @@ New things i gotta make:
 
 
 
-
-#buy_stocks(topGainers[index], True, "500")
-#clear()
-
-
-
-# We will create 2 strategies from this
-# A full short strategy based on shorting the top gainers of the first minute of the day
-
-
-
-# What the code should check
-
-#You should check the volume, bid/ask spread and depth-of-book pre-market. That big price move you are seeing may possibly just be the cost of crossing the spread pre-market. I am guessing this is a big part of the "alpha" you are seeing.
-#1#Here's a relatively simple strategy to try to see if you can make money on this:
-#
-#Calculate market beta on all the stocks you are tracking
-#
-#See how much the market moved before the open. Perhaps there is a popular index ETF to track the market you are following
-#
-#Place a limit order to sell short at the open. For each stock, set the limit order to (yesterday's close for stock) * (1.01 + (index's return before open) * (stock's beta to the index)). This basically says that if the stock moved up more than 1% on top of what the market has returned overnight, then sell it short.
-#
-#Close the position some minutes after the opening bell. Maybe 5?
-#
-#If it works don't forget who your friends are. Get in touch and I'll tell you where to deliver the yacht.
